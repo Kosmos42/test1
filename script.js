bea6a349f397ba990106b6aa82a4b7f8aaddf949
Adlet Botakhanov <botahanov@gmail.com>
2019-05-16 00:02:26 +0600

    Form validation

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function takeFormAndSum() {
-  let form = document.forms.sum;
+  let form = document.forms[0];
 
-
-  alert(form.firstNumber.value);
-  alert(form.secondNumber.value);
+  if (isNaN(parseFloat(form.firstNumber.value)))
+  {
+    alert("В первом поле не число");
+    return false
+  }
+  if(isNaN(parseFloat(form.secondNumber.value)))
+  {
+    alert("Во втором поле нет числа");
+    return false;
+  }
   return false;
 }
