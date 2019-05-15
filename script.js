c9920ad62abb64edce91f7b5fe05ea45be4c2e8e
Adlet Botakhanov <botahanov@gmail.com>
2019-05-16 01:33:28 +0600

    Sum form and solve 0.30000004 problem

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 function takeFormAndSum() {
   let form = document.forms[0];
-
-  if (isNaN(parseFloat(form.firstNumber.value)))
+  const parsedFirstNumber = parseFloat(form.firstNumber.value);
+  const parsedSecondNumber = parseFloat(form.secondNumber.value);
+  if (isNaN(parsedFirstNumber))
   {
     alert("В первом поле не число");
     return false
   }
-  if(isNaN(parseFloat(form.secondNumber.value)))
+  if(isNaN(parsedSecondNumber))
   {
     alert("Во втором поле нет числа");
     return false;
   }
-  return false;
+  // 3.00000004 problem avoid. Finding number after point and formating number using toFixed
+  const lengthAfterPointFirstNumber = form.firstNumber.value.replace("0.","").length;
+  const lengthAfterPointSecondNumber = form.secondNumber.value.replace("0.","").length;
+  let maxLengthToFix;
+  if (lengthAfterPointFirstNumber > lengthAfterPointSecondNumber)
+  {
+    maxLengthToFix = lengthAfterPointFirstNumber;
+  } else
+  {
+    maxLengthToFix = lengthAfterPointSecondNumber;
+  }
+  let sum = (parsedFirstNumber + parsedSecondNumber).toFixed(maxLengthToFix);
+  alert("Результат\n\n"+sum);
+  return true;
 }
